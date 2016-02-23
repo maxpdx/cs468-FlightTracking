3075ca7919c6b8c16d29d6e7165d5c30e8b64237
Max Litvinenko <lmaksim@pdx.edu>
2016-02-23 14:03:27 -0800

    Added IOgenerate.py with helper functions to: 	read and write files, 	generate custom length random string of ( letters, digits, custom chars, etc) 	generate random days of the week 	generate time() Added flightsPlanes.py that generates SQL for 	flight.sql, airVehicle.sql, fliesOn.sql

---
+++ IOgenerate.py
@@ -0,0 +1,66 @@
+from random import randint, SystemRandom, choice
+import string
+
+
+def get_contents(file_path="input.dat"):
+    rows = []
+    f = open(file_path, "r")
+    with f as file:
+        for line in file:
+            if line.startswith("#"):
+                continue
+            rows.append(parse(line))
+    f.close()
+    return rows
+
+
+def write_contents(lists=[], file_path="output.dat"):
+    f = open(file_path, "w+")
+
+    for line in lists:
+        f.write(str(line) + "\n")
+
+    f.close()
+
+
+def parse(line):
+    line = line.replace("\r", "")
+    line = line.replace("\n", "")
+    line = line.split(",")
+    return line
+
+
+def random_(length=4, sample=string.ascii_uppercase + string.digits):
+    """
+    Returns a random string of given length and sample
+    :param length: how log the string should be
+    :param sample: See https://docs.python.org/2/library/string.html for
+                    string vars. Some useful examples: ascii_letters,
+                    ascii_lowercase, ascii_uppercase, digits, hexdigits
+    :return: [str]
+    """
+    return ''.join(SystemRandom().choice(sample) for _ in range(length))
+
+
+def random_week_days(days=2):
+    result = ""
+    sample = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"]
+    # for x in range(length):
+    while len(result) < days * 2:
+        r = choice(sample)
+        sample.remove(r)
+        result += r
+    return result
+
+
+def random_time():
+    first = random_(1, "012")
+    if first != "2":
+        second = random_(1, string.digits)
+    else:
+        second = random_(1, "0123")
+
+    third = random_(1, "012345")
+    fourth = random_(1, string.digits)
+
+    return first + second+ ":" + third + fourth
---
+++ flightsPlanes.py
@@ -0,0 +1,89 @@
+from IOgenerate import *
+
+sourceFlight = "data/routes2.dat"
+outputFlight = "sql/flight.sql"
+outputAirVehicle = "sql/airVehicle.sql"
+outputFliesOn = "sql/fliesOn.sql"
+
+flightId = 1    # Start value and increment from there
+airVehicleId = 1
+fliesOnId = 1
+
+flightSQL = []
+airVehicleSQL = []
+fliesOnSQL = []
+
+routes = get_contents(sourceFlight)
+
+# first row
+flightSQL.append(
+    "INSERT INTO Flight (FlightId, FlightNo, AirlineId, FromAirportId, " +
+    "ToAirportId, NumOfStops, WeekDays, DepartureTime, ArrivalTime, " +
+    "Distance, Duration) VALUES")
+
+airVehicleSQL.append("INSERT INTO AirVehicle (PlaneId, TypeNum) VALUES")
+
+fliesOnSQL.append("INSERT INTO " +
+                  "FliesOn(FlightAirVehicleId, FlightId, PlaneId) VALUES")
+
+flightNOs = {}
+planes = {}
+for route in routes:
+
+    # Skipping some routes that have \N in their rows.
+    skip = False
+    for el in route:
+        if el == "\\N":
+            skip = True
+    if skip:
+        continue
+
+    # Making sure there are no duplicates in flight numbers
+    flightNO = ""
+    while flightNO == "" or flightNO in flightNOs:
+        flightNO = route[0] + "-" + random_(randint(3, 5), string.digits)
+
+    flightNOs[flightNO] = flightNO
+
+    flightSQL.append("(" +
+        str(flightId) + ", " +                          # FlightId
+        flightNO + ", " +                               # FlightNo
+        route[1] + ", " +                               # AirlineId
+        route[3] + ", " +                               # FromAirportId
+        route[5] + ", " +                               # ToAirportId
+        route[7] + ", " +                               # NumOfStops
+        random_week_days(randint(1, 4)) + ", " +        # WeekDays
+        random_time() + ", " +                          # DepartureTime
+        random_time() + ", " +                          # ArrivalTime
+        random_(randint(2, 4), string.digits) + ", " +  # Distance
+        random_time() +                                 # Duration
+        "), ")
+
+    planesList = route[8].split(" ")
+    for plane in planesList:
+        if plane not in planes:
+            planes[plane] = airVehicleId
+            airVehicleId += 1
+
+        fliesOnSQL.append("(" +
+            str(fliesOnId) + ", " +
+            str(flightId) + ", " +
+            str(planes[plane]) +
+            "),")
+        fliesOnId += 1
+    flightId += 1
+
+
+for plane in planes:
+    airVehicleSQL.append("(" +
+        str(planes[plane]) + ", " +
+        plane +
+        "),")
+
+print(outputFlight + " [" + str(len(flightSQL) - 1) + "]")
+print(outputFliesOn + " [" + str(len(fliesOnSQL) - 1) + "]")
+print(outputAirVehicle + " [" + str(len(airVehicleSQL) - 1) + "]")
+
+write_contents(flightSQL, outputFlight)
+write_contents(fliesOnSQL, outputFliesOn)
+write_contents(airVehicleSQL, outputAirVehicle)
